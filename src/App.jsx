35f29460c8a1df7f4b550b26a56081e005d15314
Elizaveta Kritskaya <post.kri@gmail.com>
2023-04-20 21:35:42 +0300

    add app + index

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import {Header} from "./Header";
+import {BestSellers} from "./BestSellers";
+
+function App() {
+  return (
+   <>
+       <div className="appContainer">
+           <Header/>
+           <BestSellers/>
+       </div>
+   </>
+  );
+}
+
+export default App;
